afc0c374dfb706efa233192c57d0f7686d1e0802
Mireyade <mireya.dlcr@gmail.com>
2022-10-11 10:40:11 -0500

    Validation to check if the vote session is still active or not

--- votes.sol
+++ votes.sol
@@ -2,23 +2,12 @@
 
 // "SPDX-License-Identifier: MIT"
 pragma solidity ^0.8.0;
-// Contrato de votaciones
-    // registrar candidatos
-    // persmiso a alguien de votar
-    // votar
-    
-
-    // Posibles Mejoras 
-    // crear multiples votaciones
-    // pasar los candidatos por argumentos del constructor
-    // agregar un token ERC20 como el derecho a votar (Recordando que los ERC20 son solo balances) y una vez que vote, quemar ese ERC20 del votante
-    // añadir un tiempo final para votar como en la vida real de las 6 de la tarde de determinado día
 
 contract Votes{
-    // variables
+
     uint256 totalVotos;
     address owner;
-    bool status; //TRUE if the voting process is still valid. FALSE if has already ended.
+    uint public time = block.timestamp; // Get the time when the contract started
 
   struct Candidato{
 
@@ -49,20 +38,6 @@ constructor(string[] memory _nombre) {
         }
     }
 
-
-/*
-    Candidato public canditato1 = Candidato({
-        id: 1,
-        nombre: "Mauricio Pinon",
-        votos: 0
-    });
-
-    Candidato public canditato2 = Candidato({
-        id: 2,
-        nombre: "Robert Carrichi",
-        votos: 0
-    });
-*/
     struct Votante{
         bool voto;
         bool puedeVotar;
@@ -71,27 +46,25 @@ constructor(string[] memory _nombre) {
     mapping(address => Votante) vota;
 
     modifier validarOwner{
-        require(msg.sender==owner, "Tu no eres el INE Dex ");
+        require(msg.sender==owner, "No eres el owner, no tienes permisos");
         _;
         
     } 
-     
+
+    //Función Permiso para votar (Sólo el owner puede ejecutar)
       function permisoVotar(address _Votante) public validarOwner() {
-       require(!vota[_Votante].puedeVotar, "Tu derecho a votar fue asignado previamente");
+       require(!vota[_Votante].puedeVotar, "Tu derecho a votar ya fue asignado previamente");
         vota[_Votante].puedeVotar= true;
         vota[_Votante].voto= false;
 
     }
 
-   
-         // funciones
-    // darle permiso a laguien de votar - (Solo el INE DEX puede ejecutar)
-   
-// votar - solo la ejecutan los votantes con permiso
+    //Función Votar (Sólo la ejecutan los votantes con permiso)
     function vote(uint _Candidato) external {
     Votante storage sender = vota[msg.sender];
         require(sender.puedeVotar, "No tienes derecho a votar");
         require(!sender.voto, " No puedes volver a votar");
+        require(time == time + 60, "Las votaciones han finalizado"); //Se le suma cierto tiempo (en segundos) al momento en que inició el contrato
         
         sender.puedeVotar = false;
         sender.voto = true;
@@ -100,19 +73,18 @@ constructor(string[] memory _nombre) {
 
     }
 
-
-//El conteo de votos se hace automático por medio de esta función:
-function validarWinner() public view
+    //El conteo de votos se hace automático por medio de esta función:
+    function validarWinner() public view
             returns (uint winner)
-    {
-        uint conteoVotos = 0;
-        for (uint counter = 0; counter < candidatos.length; counter++) {
+      {
+            uint conteoVotos = 0;
+         for (uint counter = 0; counter < candidatos.length; counter++) {
             if (candidatos[counter].votos > conteoVotos) {
                 conteoVotos = candidatos[counter].votos;
                 winner = counter;
             }
         }
-    }
+     }
 
   function ganadorAbsolutp() external view returns(string memory , address)         
     {
@@ -122,27 +94,5 @@ function validarWinner() public view
 
         return (ganador, contractAddress);
     }
-
-
- /*   function votar(uint256 _candidato) public {
-       require(vota[msg.sender].puedeVotar, "No tienes derecho a votar");
-       require(!vota[msg.sender].voto, " No puedes volver a votar");
-       
-
-        vota[msg.sender].voto=true;
-        totalVotos +=1;
-
-        if(_candidato == 1){
-            // votas por mau
-            canditato1.votos += 1;
-        } else{
-            // carrichi
-            canditato2.votos += 1;
-        }
-
-    } */
-
-  
-
 
 }
